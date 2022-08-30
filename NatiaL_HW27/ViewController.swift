3ab35b95558f5f4f4dcc4e6d4198fd8a68f5d722
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-08-30 14:02:59 +0400

    Initial Commit

---
+++ NatiaL_HW27/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatiaL_HW27
+//
+//  Created by Natia's Mac on 30.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
